1eba749e3211daeee7e3be6412b6e305b9a6984f
ggong00 <103275351+ggong00@users.noreply.github.com>
2022-04-18 23:21:15 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 176 ms, Memory: 15604 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		//입력
+		int n = Integer.parseInt(br.readLine());
+		double total =0;
+		//입력
+		for(int i =0; i<n; i++) {
+			int count =0;
+			double sum = 0;
+			st = new StringTokenizer(br.readLine() , " ");
+			int nn = Integer.parseInt(st.nextToken());
+			int[] array = new int[nn]; 
+			for(int j=0; j<array.length; j++) {			
+				array[j] = Integer.parseInt(st.nextToken()); 
+				sum += array[j];
+			}
+			total = sum/nn;
+			for(int c =0; c<array.length; c++) {
+				if(array[c] > total) {
+					count += 1;
+				}
+
+			}
+			bw.write(String.format("%.3f", (double)count*100/nn) + "%\n");	
+		}
+		bw.flush();
+		bw.close();
+	}	
+}
