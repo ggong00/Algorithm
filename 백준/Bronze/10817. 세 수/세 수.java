2310d9bd3fc8544607237593f45d9be8d3f7a124
ggong00 <103275351+ggong00@users.noreply.github.com>
2022-06-23 21:10:10 +0900

    [Bronze III] Title: 세 수, Time: 220 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(med(a, b, c));
+    }
+
+    public static int med(int a, int b, int c) {
+        if (a >= b) {
+            if (b >= c) {
+                return b;
+            } else if (a <= c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else if (a > c) {
+            return a;
+        } else if (c > b) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+}
